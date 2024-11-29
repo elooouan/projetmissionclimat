7c8d3e98ce911ca1e45cd2da7856824b70b3e493
elouan <145717537+elooouan@users.noreply.github.com>
2024-11-30 00:23:21 +0100

    Update Q1.py

--- actions/Q1.py
+++ actions/Q1.py
@@ -10,16 +10,16 @@ class Window(tk.Toplevel):
         display.centerWindow(600, 400, self)
         self.title('Q1 : départements de la zone climatique H1')
         display.defineGridDisplay(self, 2, 1)
-        ttk.Label(self,
-                  text="Modifier cette fonction en s'inspirant du code de F1, pour qu'elle affiche la liste des "
-                       "départements (code_departement, nom_departement) de la zone climatique H1",
-                  wraplength=500,
-                  anchor="center",
-                  font=('Helvetica', '10', 'bold')
-                  ).grid(sticky="we", row=0)
-
+        
         #TODO Q1 Modifier la suite du code (en se basant sur le code de F1) pour répondre à Q1
 
         # On définit les colonnes que l'on souhaite afficher dans la fenêtre et la requête
+        columns = ('code_departement', 'nom_departement')
+        query = """SELECT code_departement, nom_departement
+                    FROM Departements JOIN Mesures USING (code_departement) 
+                    WHERE zone_climatique = 'H1'
+                """
 
         # On utilise la fonction createTreeViewDisplayQuery pour afficher les résultats de la requête
+        tree = display.createTreeViewDisplayQuery(self, columns, query,200)
+        tree.grid(row=0, sticky="nswe")
